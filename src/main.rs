f29a4b3b62c7c16f49fe87ca272ff41334592f1a
Kasil <kasil@loutre.info>
2022-12-05 08:01:27 +0100

    day 05

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,13 @@ mod day_01;
 mod day_02;
 mod day_03;
 mod day_04;
+mod day_05;
 
 fn main() -> Result<(), std::io::Error>  {
     day_01::run()?;
     day_02::run()?;
     day_03::run()?;
     day_04::run()?;
+    day_05::run()?;
     Ok(())
 }
